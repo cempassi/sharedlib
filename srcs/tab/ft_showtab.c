d5086253960dbafde94efaed3452594806742c89
Cedric M'PASSI <cempassi@student.42.fr>
2019-05-04 00:42:52 +0200

    Showtab protection

--- srcs/tab/ft_showtab.c
+++ srcs/tab/ft_showtab.c
@@ -6,7 +6,7 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 19:49:20 by cempassi          #+#    #+#             */
-/*   Updated: 2019/04/23 19:58:57 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/05/04 00:42:08 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_showtab(char **t)
 {
+	if (!t || !*t)
+		return ;
 	while (*t)
 		ft_printf("%s|",*t++);
 	ft_putchar('\n');
